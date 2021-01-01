f31b20d8a41eb816cb2240591d0953f3a9d4f884
Daniela C. Barbosa <contato@danielacb.com>
2021-01-01 17:14:46 -0300

    markdown-editor: finish code

--- markdown-editor/src/App.js
+++ markdown-editor/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import "./App.css";
 
 export default function App() {
+  const [markdown, setMarkdown] = useState("# Title");
+
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  }
+
   return (
     <div className="app">
-      <textarea />
-
-      <div className="preview" />
+      <textarea onChange={handleChange} placeholder={markdown} />
+      <ReactMarkdown className="preview" source={markdown} />
     </div>
   );
 }
