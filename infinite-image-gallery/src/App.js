e3921018e8ea744e2f01eee283814f4c9ff1a6e1
Daniela C. Barbosa <contato@danielacb.com>
2021-01-06 00:13:46 -0300

    infinite image gallery: finish code

--- infinite-image-gallery/src/App.js
+++ infinite-image-gallery/src/App.js
@@ -1,23 +1,85 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import "./App.css";
+
+const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 
 export default function App() {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    getPhotos();
+    // eslint-disable-next-line
+  }, [page]);
+
+  function getPhotos() {
+    let url;
+    query
+      ? (url = `https://api.unsplash.com/search/photos?query=${query}`)
+      : (url = `https://api.unsplash.com/photos?`);
+
+    url += `&page=${page}`;
+    url += `&client_id=${accessKey}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const results = data.results ?? data;
+        page === 1
+          ? setImages(results)
+          : setImages((images) => [...images, ...results]);
+      });
+  }
+
+  function searchPhotos(e) {
+    e.preventDefault();
+    setPage(1);
+    getPhotos();
+  }
+
+  if (!accessKey) {
+    return (
+      <a href="https://unsplash.com/developers" className="error">
+        Required: get your Unsplash API Key first!
+      </a>
+    );
+  }
+
   return (
     <div className="app">
       <h1>Unsplash Image Gallery!</h1>
 
-      <form>
-        <input type="text" placeholder="Search Unsplash..." />
+      <form onSubmit={searchPhotos}>
+        <input
+          type="text"
+          placeholder="Search Unsplash..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
         <button>Search</button>
       </form>
 
-      <div className="image-grid">
-        {[...Array(100)].map((_, index) => (
-          <div className="image" key={index}>
-            <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
-          </div>
-        ))}
-      </div>
+      <InfiniteScroll
+        dataLength={images.length}
+        next={() => setPage((page) => page + 1)}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        <div className="image-grid">
+          {images.map((image, index) => (
+            <a
+              className="image"
+              key={index}
+              href={image.links.html}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={image.urls.regular} alt={image.alt_description} />
+            </a>
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 }
