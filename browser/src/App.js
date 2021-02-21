c39b44f6397a1fd18ee0e8d71dca5939589027fe
Daniela C. Barbosa <contato@danielacb.com>
2021-02-20 23:48:11 -0300

    browser: add and select tabs

--- browser/src/App.js
+++ browser/src/App.js
@@ -10,20 +10,35 @@ export default function App() {
   ]);
   const [activeBrowser, setActiveBrowser] = useState(0);
 
+  function chooseBrowser(id) {
+    setActiveBrowser(id);
+  }
+
+  function addBrowser() {
+    const newBrowsers = [...browsers, ""];
+    setBrowsers(newBrowsers);
+    setActiveBrowser(newBrowsers.length - 1);
+  }
+
   const url = browsers[activeBrowser];
 
   return (
     <div className="app">
       <div className="browser">
-        <Tabs browsers={browsers} active={activeBrowser} />
+        <Tabs
+          browsers={browsers}
+          active={activeBrowser}
+          choose={chooseBrowser}
+          add={addBrowser}
+        />
 
         <AddressBar />
 
         <div className="viewport">
           {url ? (
-            <iframe src={url} frameborder="0" title="viewport" />
+            <iframe src={url} frameBorder="0" title="viewport" />
           ) : (
-            <>Open a new Tab!</>
+            <>Brand new Tab!</>
           )}
         </div>
       </div>
