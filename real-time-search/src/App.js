3af227be6e5852d125da1ca142dc78d7682ca911
Daniela C. Barbosa <contato@danielacb.com>
2021-03-06 00:22:22 -0300

    real-time search: setup algolia

--- real-time-search/src/App.js
+++ real-time-search/src/App.js
@@ -1,5 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+
+import algoliasearch from "algoliasearch/lite";
+import {
+  InstantSearch,
+  SearchBox,
+  Hits,
+  Pagination,
+  Stats,
+} from "react-instantsearch-dom";
+
+import "./App.css";
 
 // ===== OPTION 1: if you want to create your own algolia account and fill it with data =====
 // demo data: https://www.algolia.com/doc/guides/building-search-ui/resources/demos/react/#media
@@ -10,10 +20,40 @@ import './App.css';
 // api key: ae8a3f7cff93d40282a4831930bfa38b
 // index name: products
 
+const searchClient = algoliasearch(
+  process.env.REACT_APP_ALGOLIA_APP_ID,
+  process.env.REACT_APP_ALGOLIA_API_KEY
+);
+
 export default function App() {
   return (
-    <div className="app">
-      <div className="search-container"></div>
-    </div>
+    <InstantSearch searchClient={searchClient} indexName="products">
+      <div className="app">
+        <div className="search-container">
+          <Stats />
+          <SearchBox />
+          <Hits hitComponent={Product} />
+          <Pagination />
+        </div>
+      </div>
+    </InstantSearch>
+  );
+}
+
+export function Product({ hit }) {
+  return (
+    <a
+      href={hit.url}
+      className="product"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src={hit.image} alt={hit.name} />
+      <div>
+        <h3>{hit.brand}</h3>
+        <h2>{hit.name}</h2>
+        <p>${hit.price}</p>
+      </div>
+    </a>
   );
 }
