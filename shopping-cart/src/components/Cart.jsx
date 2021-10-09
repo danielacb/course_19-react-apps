f31e43deff7bdfb89c687d2594469554397c6ef4
Daniela Barbosa <contato@danielacb.com>
2021-10-09 16:16:58 -0300

    shopping-cart: finish context code

--- shopping-cart/src/components/Cart.jsx
+++ shopping-cart/src/components/Cart.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { useCart } from "../contexts/useCart";
 
 export default function Cart() {
+  const { addItem, removeItem, cartGroupedByItems, totalPrice } = useCart();
+
   return (
     <div className="cart">
       {/* show cart items here */}
-
-      <div className="total">$100</div>
+      {cartGroupedByItems.length > 0 ? (
+        cartGroupedByItems.map((product) => (
+          <div className="cart-item" key={product.sku}>
+            <img src={product.image_url} alt={product.name} />
+            <div className="content">
+              <h3>{product.name}</h3>
+              <div className="cart-buttons">
+                <button onClick={() => removeItem(product.sku)}>-</button>
+                <button>{product.quantity}</button>
+                <button onClick={() => addItem(product.sku)}>+</button>
+              </div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div className="cart-item">
+          <h3>Your cart is empty</h3>
+        </div>
+      )}
+      <div className="total">${totalPrice()}</div>
     </div>
   );
 }
--- shopping-cart/src/contexts/useCart.jsx
+++ shopping-cart/src/contexts/useCart.jsx
@@ -13,6 +13,7 @@ function reducer(state, { type, payload }) {
         ...state,
         cart: [...state.cart, products.find((p) => p.sku === payload)],
       };
+
     case "REMOVE":
       const indexInCart = state.cart.findIndex(
         (product) => product.sku === payload
@@ -20,6 +21,10 @@ function reducer(state, { type, payload }) {
       const newCart = [...state.cart];
       newCart.splice(indexInCart, 1);
       return { ...state, cart: newCart };
+
+    case "EMPTY":
+      return { cart: [] };
+
     default:
       return state;
   }
@@ -38,13 +43,37 @@ export function CartProvider({ children }) {
     return itemsInCart.length;
   }
 
+  function groupCartItems() {
+    return state.cart.reduce((newCart, product) => {
+      const indexInCart = newCart.findIndex((p) => p.sku === product.sku);
+      const isInCart = indexInCart !== -1;
+
+      if (isInCart) {
+        newCart[indexInCart].quantity++;
+      } else {
+        newCart.push({ ...product, quantity: 1 });
+      }
+
+      return newCart;
+    }, []);
+  }
+
+  function totalPrice() {
+    return groupCartItems().reduce((totalPrice, product) => {
+      return totalPrice + product.price * product.quantity;
+    }, 0);
+  }
+
   return (
     <CartContext.Provider
       value={{
         addItem,
         removeItem,
         cart: state.cart,
+        cartGroupedByItems: groupCartItems(),
         countItemsInCart,
+        totalPrice,
+        emptyCart,
       }}
     >
       {children}
