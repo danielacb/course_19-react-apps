cfa9e162e76f81cbe2ae9db137a0c12038b21ff2
Daniela Barbosa <contato@danielacb.com>
2021-10-12 21:47:48 -0300

    typing-tester: create type comparison feature

--- typing-tester/src/App.jsx
+++ typing-tester/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 // to calculate typing speed
@@ -8,7 +8,24 @@ import "./App.css";
 const secondsToCount = 10;
 const paragraph = `Coding is the best. We are able to build something from scratch. It is literally imagination incarnate. Solving our own problems through coding is one of the coolest things we could do!`;
 
+function findTypos(str1, str2) {
+  let typos = [];
+
+  str2.split("").forEach((char, index) => {
+    if (char !== str1.charAt(index)) typos.push(index);
+  });
+
+  return typos;
+}
+
 export default function App() {
+  const [typedText, setTypedText] = useState("");
+  const [typoIndexes, setTypoIndexes] = useState([]);
+
+  useEffect(() => {
+    setTypoIndexes(findTypos(paragraph, typedText));
+  }, [typedText]);
+
   return (
     <div className="app">
       {/* sidebar */}
@@ -20,11 +37,30 @@ export default function App() {
 
       <div className="content">
         {/* show the paragraph */}
-        <p>{paragraph}</p>
+        <p>
+          {paragraph.split("").map((char, index) => {
+            let charClass = "";
+            const hasBeenTyped = typedText.length > index;
+
+            if (hasBeenTyped)
+              charClass = typoIndexes.includes(index) ? "incorrect" : "correct";
+
+            return (
+              <span key={index} className={charClass}>
+                {char}
+              </span>
+            );
+          })}
+        </p>
 
         {/* show the textarea */}
         <form>
-          <textarea rows="10" placeholder="Test your typing skills..." />
+          <textarea
+            rows="10"
+            placeholder="Test your typing skills..."
+            value={typedText}
+            onChange={(e) => setTypedText(e.target.value)}
+          />
         </form>
       </div>
     </div>
